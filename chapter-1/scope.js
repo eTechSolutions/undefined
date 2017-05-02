40720f055fff5c9babd82462202e733da1ad6b6b
Robert Gill <envman@gmail.com>
2017-05-02 15:53:18 +0100

    add scope

--- chapter-1/scope.js
+++ chapter-1/scope.js
@@ -5,8 +5,14 @@ if (t) {
   console.log(a)
 }
 
-console.log(a)
+console.log(a) // does this work?
 
 let f = function() {
   var b = 'hello'
 }
+
+console.log(b) // does this work?
+
+// Try this
+// input above
+// run, why does it work this way?
