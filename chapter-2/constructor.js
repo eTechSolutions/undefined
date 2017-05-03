cdf264ff5873e0ad32c0e18517f3af53ff94d9ce
TPitchers <torinpitchers@etech.net>
2017-05-03 15:55:53 +0100

    Updated Constructor.js
    Added Narrative.

--- chapter-2/constructor.js
+++ chapter-2/constructor.js
@@ -1,11 +1,23 @@
-let Car = function() {
+//define constructor for cars with an engineSpeed parameter, then set object properties using 'this'
+let Car = function(engineSpeed) {
 
-  this.engine = 'fast'
+  this.engine = engineSpeed
   this.lol = function() {
     console.log(this.engine)
   }
 }
 
-let car = new Car()
+//create a new car with a fast engine
+let fastCar = new Car("fast")
+//create a new car with a slow engine
+let slowCar = new Car("slow")
 
-car.lol()
+//output engine speeds
+fastCar.lol()
+slowCar.lol()
+
+//Now Try
+//define a constructor for dogs with a parameter for the breed, and store in a property
+//add a function to display the breed
+//create a pitbull and a bulldog and output their breed using the function you created
+
